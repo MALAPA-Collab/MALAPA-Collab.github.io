a6c86f5fdfd9459137ac10ab53febf2b46db5e77
Andrea Santamaria Garcia <ansantam@liverpool.ac.uk>
2025-04-10 10:34:52 +0200

    Set base to root for GitHub Pages

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,5 +2,5 @@ import { defineConfig } from 'astro/config';
 
 export default defineConfig({
   output: 'static',
-  base: '/MALAPA-Collab.github.io/', // Adjust if your repository is not using a custom domain
+  base: '/',  // root — because it's an org/user GitHub Pages site
 });
